a0431d69c033f14c4d555d473ae1364ab2a2ca7a
hannerc <31261156+hannerc@users.noreply.github.com>
2018-03-20 23:09:41 -0700

    nn

--- server.js
+++ server.js
@@ -3,20 +3,15 @@ var cheerio = require("cheerio");
 var request = require("request");
 var express = require("express");
 var mongojs = require("mongojs");
+var mongoose = require("mongoose");
 
 // Initialize Express
 var app = express();
 
 // Database configuration
 // Save the URL of our database as well as the name of our collection
-var databaseUrl = "";
-var collections = [""];
-// Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+var databaseUrl = "mongoHeadlines";
+var collections = ["headlines"];
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -26,6 +21,70 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true
+});
+
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request for the news section of trekmovie
+  request("http://trekmovie.com/", function(error, response, html) {
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+
+    // For each element with a "title" class
+    $(".content-thumb").each(function(i, element) {
+      // Save the text and href of each link enclosed in the current element
+      var title = $(element).children("a").attr("title");
+      var link = $(element).children("a").attr("href");
+      
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
+    });
+  });
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
 });
 
 // Set the app to listen on port 3000
